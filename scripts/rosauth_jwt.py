6024c41bc3a5d99cb124afc73e0e533e0d0a3e07
Chris Paliqaw <christophomos@gmail.com>
2023-01-19 23:36:49 +0800

    feature: extract email and user-groups from jwt

--- scripts/rosauth_jwt.py
+++ scripts/rosauth_jwt.py
@@ -7,15 +7,21 @@ from robotnik_msgs.srv import VerifyJwt, VerifyJwtRequest, VerifyJwtResponse
 from dotenv import load_dotenv
 
 class RosauthJwt():
-    SERVICE_NAME = '/verify_jwt'
+    SERVICE_NAME = "verify_jwt"
+    JWT_ALGORITHM = "JWT_ALGORITHM"
+    JWT_AUDIENCE = "JWT_AUDIENCE"
     JWT_KEY = "JWT_KEY"
+    HEADER_DATA_ALG_KEY = "alg"
+    DECODED_TOKEN_EMAIL_KEY = 'email'
+    DECODED_TOKEN_APP_METADATA_KEY = "app_metadata"
+    DECODED_TOKEN_USER_GROUPS_KEY = "user-groups"
     
     def __init__(self):
         load_dotenv()
         # In Supabse, this is found in Project Settings -> API -> JST Secret
         self.key: str = os.environ.get(RosauthJwt.JWT_KEY)
-        self.alg: str = os.environ.get("JWT_ALGORITHM")
-        self.audience: str = os.environ.get("JWT_AUDIENCE")
+        self.alg: str = os.environ.get(RosauthJwt.JWT_ALGORITHM)
+        self.audience: str = os.environ.get(RosauthJwt.JWT_AUDIENCE)
         self.service = rospy.Service(RosauthJwt.SERVICE_NAME, VerifyJwt, self.handle_authenticate_token)
         self.ctrl_c = False
         rospy.on_shutdown(self.shutdownhook)
@@ -31,10 +37,10 @@ class RosauthJwt():
         # https://auth0.com/blog/how-to-handle-jwt-in-python/
         try:
             header_data = jwt.get_unverified_header(request.token)
-            rospy.logwarn(f"{header_data['alg']=}")
+            rospy.logwarn(f"{header_data[RosauthJwt.HEADER_DATA_ALG_KEY]=}")
             # Best to know alg: https://en.wikipedia.org/wiki/JSON_Web_Token#Vulnerabilities
-            if (header_data['alg'] != self.alg):
-                raise ValueError(f"Unexpected JST algorith: expected {self.alg}, but was {header_data['alg']}")
+            if (header_data[RosauthJwt.HEADER_DATA_ALG_KEY] != self.alg):
+                raise ValueError(f"Unexpected JST algorith: expected {self.alg}, but was {header_data[RosauthJwt.HEADER_DATA_ALG_KEY]}")
             # using that variable in the decode method
             decoded_token = jwt.decode(
                 request.token,
@@ -42,27 +48,23 @@ class RosauthJwt():
                 audience=self.audience,
                 algorithms=[self.alg])
                 # TODO: add JWT options
-
-            # Token is valid and not revoked.
-            rospy.logwarn(f"{decoded_token['email']=}")
-            # user_group_key = 'user-level'
-            # rospy.logwarn(decoded_token.keys())
-            if True:# user_group_key in decoded_token:
-                # user_group_value = decoded_token[user_group_key]
-                # rospy.logwarn(f"{user_group_value=}")
+            # Token is valid and not expired
+            email: str = decoded_token[RosauthJwt.DECODED_TOKEN_EMAIL_KEY]
+            rospy.logwarn(f"{email=}")
+            user_groups = decoded_token[RosauthJwt.DECODED_TOKEN_APP_METADATA_KEY][RosauthJwt.DECODED_TOKEN_USER_GROUPS_KEY]
+            rospy.logwarn(f"{user_groups=}")
+            if user_groups:
                 response.authenticated = True
-                # response.user_group = user_group_value
+                response.email = email
+                response.user_groups = user_groups
             else:
-                rospy.logerr("User has no group")
-                # auth.set_custom_user_claims(uid, {user_group_key: 'monitor'})
+                rospy.logerr("User has no user-groups")
                 response.authenticated = False
-                response.user_group = 'none'# VerifyJwt.USER_GROUP_NONE
 
         except Exception as e:
-            # Token is revoked. Inform the user to authenticate again.
+            # Token is revoked, etc.
             rospy.logerr(e)
             response.authenticated = False
-            response.user_group = 'none'# VerifyJwt.USER_GROUP_NONE
         return response
         
     def shutdownhook(self):
